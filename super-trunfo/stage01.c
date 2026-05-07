895305421dcf8aefdace1c4ce90c64844c4433d4
jvitorcsm <joaovitorcarvalhosavimondo@gmail.com>
2026-05-06 23:38:34 -0300

    falta finalizar alguns requisitos do stage01

---
+++ super-trunfo/stage01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    //Try in english for practive
+    //Just curious, this game is related to Brazil at this stage.
+    struct card01 {
+        state char[21];
+        code char[5]; // State abbreviation + card number. Ex: SC01
+        cityName[33];
+        population[float];
+        areaKm2[float]; 
+        gdp[float]; // Portuguese PIB -> English GDP Gross-Domestic-Product
+        touristAttractionsCount[int]
+    }
+
+    struct card02 {
+
+        state char[21];
+        code char[5]; // State abbreviation + card number. Ex: SC01
+        cityName[33];
+        population[float];
+        areaKm2[float]; 
+        gdp[float]; // Portuguese PIB -> English GDP Gross-Domestic-Product
+        touristAttractionsCount[int]
+
+    }
+
+    
+}
